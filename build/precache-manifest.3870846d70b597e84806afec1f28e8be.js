4a9f01873808d5017315daaf8c501257e261b9c4
zenniahelles <zenniahelles@gmail.com>
2020-06-19 13:04:07 +0200

    added build

---
+++ build/precache-manifest.3870846d70b597e84806afec1f28e8be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e46a3994a729f6c14eea9435af35e5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e51fd960421fdfee43f",
+    "url": "/static/css/main.018b4616.chunk.css"
+  },
+  {
+    "revision": "140f883b5973c028e063",
+    "url": "/static/js/2.0c0367cc.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.0c0367cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e51fd960421fdfee43f",
+    "url": "/static/js/main.5476346e.chunk.js"
+  },
+  {
+    "revision": "82a8471f42883f47ba40",
+    "url": "/static/js/runtime-main.f66464ee.js"
+  },
+  {
+    "revision": "2eee728e2ae123d9be96448e8bd837fc",
+    "url": "/static/media/banner.2eee728e.jpg"
+  },
+  {
+    "revision": "7c4a0e18ca73b92a830e47f129a13880",
+    "url": "/static/media/footerbg.7c4a0e18.jpg"
+  },
+  {
+    "revision": "87bf54d9d54b4b5c8c7e419c011613f6",
+    "url": "/static/media/hancock.87bf54d9.png"
+  },
+  {
+    "revision": "0d914b4644088f31439f103484952373",
+    "url": "/static/media/logo.0d914b46.jpg"
+  }
+]);
---
+++ build/precache-manifest.e5c29edc671d1e2ce0ffb42fdb741f84.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba2cec7a64146fb28839491fd43e325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b0e32e5c7907bf326c",
+    "url": "/static/css/main.77e04089.chunk.css"
+  },
+  {
+    "revision": "ff88b03deadc687c2aed",
+    "url": "/static/js/2.2af134ba.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.2af134ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b0e32e5c7907bf326c",
+    "url": "/static/js/main.04c11895.chunk.js"
+  },
+  {
+    "revision": "82a8471f42883f47ba40",
+    "url": "/static/js/runtime-main.f66464ee.js"
+  },
+  {
+    "revision": "2eee728e2ae123d9be96448e8bd837fc",
+    "url": "/static/media/banner.2eee728e.jpg"
+  },
+  {
+    "revision": "7c4a0e18ca73b92a830e47f129a13880",
+    "url": "/static/media/footerbg.7c4a0e18.jpg"
+  },
+  {
+    "revision": "87bf54d9d54b4b5c8c7e419c011613f6",
+    "url": "/static/media/hancock.87bf54d9.png"
+  },
+  {
+    "revision": "5ca0424e1ee3da5cc31a591fa2c38e53",
+    "url": "/static/media/kort.5ca0424e.jpg"
+  },
+  {
+    "revision": "a6ecb6a409c8a48bef4e2953608f0621",
+    "url": "/static/media/loginbanner.a6ecb6a4.jpg"
+  },
+  {
+    "revision": "0d914b4644088f31439f103484952373",
+    "url": "/static/media/logo.0d914b46.jpg"
+  },
+  {
+    "revision": "70cb80b0b0f1973091e7900e335b74fe",
+    "url": "/static/media/mediesuset-map.70cb80b0.jpg"
+  }
+]);
